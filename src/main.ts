64507ec292318cadba9fcc65eb85fb1d70771067
manhG <nguyenmanhict@gmail.com>
2024-07-30 01:57:22 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,38 @@
-import { Elysia } from 'elysia'
+import { Elysia, Context } from "elysia";
 
-export const app = new Elysia()
-	.get('/', () => 'Hello Elysia')
-	.listen(3000)
+const app = new Elysia()
+  .get("/", (c: Context) => {
+    if (c.query.url) {
+      try{
+        return fetch(c.query.url)
+        .then(r => r.text())
+        .then((data) => {
+          return {
+            error: false,
+            message: 200,
+            data: data
+          }
+        })
+        .catch((err:any) => {
+          return {
+            error: true,
+            message: err.message,
+            data: ""
+          }
+        });
+      }
+      catch(err:any){
+        return {
+          error: true,
+          message: err.message,
+          data: ""
+        }
+      }
+    }
+    else return 'OK';
+  })
+  .listen(2000);
 
-console.log(`🦊 Elysia is running at on port ${app.server?.port}...`)
+  console.log(
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  );
